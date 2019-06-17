a7d61f28b040f5effa048a0e6ef86f083f115822
Jon B <jonbchi@pm.me>
2019-06-17 01:26:51 -0500

    v1 leggoooo

---
+++ heartland-site/src/pages/additional-resources/additional-resources-page.tsx
@@ -0,0 +1,12 @@
+
+import * as React from 'react';
+import Markdown from 'react-markdown';
+import Content from './content';
+
+export default () => {
+    return (
+        <div >
+            <Markdown source={Content}/>
+        </div>
+    );
+};
---
+++ heartland-site/src/pages/additional-resources/content.tsx
@@ -0,0 +1,12 @@
+
+/* tslint:disable */
+export default`
+
+[FAQ](https://drive.google.com/open?id=1AzC1SHsdLW5jB4HDjThsrNUtJfxBRDFK)
+
+[Info & Background](https://drive.google.com/open?id=1oA_d1B8U1HJE5YtN2DNwZik-Wi_zB3W8)
+
+[Letter to David Sinski](https://drive.google.com/open?id=13DGkXr3_QdnCErm6Z5-upqfx4P5k74xZ)
+
+[Petition (printable version)](https://drive.google.com/open?id=1MnOITCqPlx3u2OKCwkdgV8lg_qC9a-Mt)
+`;
