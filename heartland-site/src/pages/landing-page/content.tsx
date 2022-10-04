424436fc94810ad50a91564227c124e02780ae43
Henry <hhbalch@gmail.com>
2022-10-03 20:15:09 -0500

    landing page copy update

--- heartland-site/src/pages/landing-page/content.tsx
+++ heartland-site/src/pages/landing-page/content.tsx
@@ -1,62 +1,36 @@
 /* tslint:disable */
 
 export default `
-# COVID-19 UPDATE
-### On April 13th, ProPublica Illinois reported that Heartland Alliance is experiencing the largest COVID-19 outbreak in any detention facility for immigrant youth to date. Out of the 69 children Heartland is currently detaining, 37 children, as well as 2 employees at the detention center in Bronzeville have tested positive so far.
-
-## [You can read the ProPublica article here.](tinyurl.com/heartlandcovidoutbreak)
-
-### Anticipating this kind of an outbreak is exactly why we demanded, a month ago, that Heartland close their detention centers and release the children to their families and sponsors. Many of these detained children arrive at the Heartland centers immunocompromised and with health problems as a result of their arduous journeys to the U.S. and their experiences with violence at the hands of ICE. 
-
-### Our past interviews with former Heartland employees have revealed a pattern of refusal to give the child detainees adequate medical care. Even when Heartland was detaining 400 children at a time, there was only one doctor available to all 400 children. 
-
-### This is a terrifying, potentially deadly situation. 
-
-### There is absolutely no justification for Heartland’s continued operation of these detention centers. We demand that Heartland Alliance PERMANENTLY STOP all intake of children into their jails; REUNITE all of the children they currently detain IMMEDIATELY and WITHOUT collaborating with ICE (by collecting and sharing every child’s sponsors’ biometric information); that all infected children must be provided appropriate medication and a post-discharge treatment plan; and that all current detention center staff must be relocated within the organization.
-
-
-<div display="flex" justify-content="center" width="100%" padding-top="5px">
-<img src="https://live.staticflickr.com/65535/49807632563_0cb22a7b52_z.jpg" alt="Image from iOS">
-</div>
 <div display="flex" justify-content="center" width="100%" padding-top="5px">
 <img src="https://live.staticflickr.com/65535/49807632543_71483b5287_z.jpg" alt="Image from iOS">
 </div>
 
-## Introduction & Demands
+# Help End the Detention and Deportation Machine by Ending Child Migrant Detention Right Here In Chicago
+## Heartland Alliance detains hundreds of migrant children in Chicago every year.
+
+Heartland Alliance is a Chicago-based nonprofit that’s paid tens of millions of dollars by the US government to detain unaccompanied migrant children under their Heartland Human Care Services division. They do this under the guise of providing “shelter” for these children.
+
+## These are not shelters. They’re detention centers.
 Immigrant children who have family and sponsors in the United States are being detained as
 “unaccompanied alien children” in Chicago and the greater Chicagoland area.
 
-All detention centers in the area are and have been operated by the Chicago-based nonprofit
-organization **Heartland Alliance** (as of June, 2019).
-There are five currently operating detention centers administered by Heartland Alliance’s Human
-Care Services division under contract from the U.S. Department of Health and Human Services’ Office
-of Refugee Resettlement in direct cooperation with U.S. Immigration and Customs Enforcement (ICE).
-There are typically between 300 and 400 children detained in these facilities at any given time.
-As of July 2018,
-[ProPublica](https://www.propublica.org/article/illinois-chicago-children-teens-immigration-shelters-heartland)
-reported that, _“of the more than 100 federally contracted sites
-around the country, Heartland has received the fourth-highest amount of federal dollars for
-housing unaccompanied minors since fiscal year 2015—more money than any other organization outside Texas.”_
-Heartland Alliance claims to be providing these children safe harbor. They hold that children who enter the
-country without an accompanying adult “have to go somewhere,” arguing that Heartland’s facilities
-are preferable to one of the larger-scale detention centers with more well-publicized horrifying conditions
-like Tornillo (Texas) or Homestead (Florida). Although immigrant children were transported to these
-facilities against their or their families’ express wish and are being held at these facilities
-against their and their families’ will, Heartland describes these facilities as “shelters” instead of
-detention centers and claims that closing them would only further harm a population already under assault.
-The organization is separating children from their families and loved ones under terms dictated by ICE and
-allied federal agencies.
+Children are not allowed to leave the detention centers or even visit family/loved ones temporarily.
+Heartland has a history of working closely with ICE: deporting kids if they turn 18 under Heartland’s care and endangering the family and friends of children by reporting their information to ICE.
+There is a proven pattern of medical abuse, neglect, and traumatization at the hand of Heartland. Learn more here. (link to FAQ page) 
 
-It risks the lives of the children and their families by collaborating directly with ICE.
+## Our Goal: shut them down and #FreeTheKids.
 
-### We demand that Heartland Alliance:
-1. Immediately **cease intaking migrant children** into their detention centers.
-2. Immediately **end their contract** with the U.S. Department of Health and Human
-Services (DHHS) to detain migrant children.
-3. **Release and reunify** the migrant children in their physical custody with their
-sponsors and families in the United States as soon as possible without sharing their sponsors’
-biometric data, documentation status, or other personal information with ICE or with any federal
-agency that shares information with ICE.
-4. Pledge to use their resources and power to **fight for a true detention-free,
-deportation-free, and ICE-free zone in Chicago**.
+We’re demanding that Heartland shut down these detention centers and immediately reunite all children with their sponsors.
+Learn more about the campaign and our demands here. (link to About page)
+
+## Why #FreeTheKids?
+
+Short Term: Shutting down these detention centers will have an immediate impact on the hundred(s) of children currently detained. Majority of these children have family or friends in the U.S. ready to take them in.
+Long Term: Fewer beds = fewer detentions. The detention and deportation machine is beyond reform. We must reduce the U.S.’s capacity to detain and immediately release detainees by working across the country to close one center at a time.
+
+## Get Involved Today
+
+Want to learn more about the campaign? Interested in attending an upcoming meeting or action? Do you have a story to share about Heartland’s detention centers? Let us know.
+
+[GET INVOLVED](https://docs.google.com/forms/d/e/1FAIpQLScMl5_eTf0SL59e1-3VekgJubCLBCG2dQivKCMcvz0WkEoe-w/viewform)
 `;
